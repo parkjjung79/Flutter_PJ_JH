439186524e8d09f75a8f9037589402707856107d
parkjjung79 <parkjjung79@naver.com>
2023-12-21 02:17:23 +0900

    ss

--- 02. 다트연습/03.클래스.dart
+++ 02. 다트연습/03.클래스.dart
@@ -1,5 +1,22 @@
 void main(List<String> args) {
+
+  // Dog클래스 메모리에 생성하기
+  // 클래스를 담는 변수의 형은 그 클래스명을 사용한다!
+  Dog d1 = Dog();
+  print('나의 강아지 종류이름은 ${d1.name}이다!');
+  // 클래스의 속성값 업데이트하기
+  d1.name = '닥스훈트';
+  print('1년후 나의 새로운 강아지 종류명은 ${d1.name}이다');
+  // 중요한 것은 클래스의 속성자체의 정의다!
+  // 값은 그것을 사용하는 사용자의 등록에 따라 달라질 수 있다!
+  print('내 강아지의 색깔은 ${d1.color}이고 나이는 ${d1.age}살이다!');
   
+  print('훈련1회 실시함! 물기지수 ${d1.trainingDog()%}');
+  dynamic doit;
+  for(var i=0;i<10;i++){
+    doit = d1.trainingDog();
+  }
+
 }
 
 /*************************************** 
@@ -12,8 +29,14 @@ void main(List<String> args) {
 
 형식 : class 키워드를 사용하여 파스칼 케이스로 명명함
 class MyClassIsPerfect{
-  코드...
+  변수선언 및 할당
+  생성자메서드(){} -> 반드시 실행이된다
+  메서드() -> 구성품
 }
+-> 변수는 선언만 할 수도 있고 할당까지 할 수 있음
+-> 생성자메서드는 클래스 사용시 가장먼저 호출되는 메서드
+-> 개별적인 클래스 구성 메서드 생성할 수 있음
+
 ***************************************/
 
 // 멍멍이 클래스
@@ -22,11 +45,18 @@ class Dog{
   String name = "시바견";
   int age = 8;
   String color = "검정색";
+  // 물기지수
   int bite = 100;
 
   // 클래스 메서드
-  void trainingDog(){
+  int trainingDog(){
     bite = bite - 5;
+    // 5%100% 값 한계 설정
+    if(bite<5) bite = 5;
+    else if(bite>100) bitte = 100;
+    
+    //리턴값
+    return bite;
   }
 
 } //////// Dog 클래스 ////////
